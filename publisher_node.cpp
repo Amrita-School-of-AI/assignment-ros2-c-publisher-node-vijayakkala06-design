06fa2f8bbddcb35939210f22cff05e0f6a57edb5
Vijay Kiran Reddy <vijayakkala06@gmail.com>
2026-02-01 15:01:57 +0530

    Completed ROS2 Publisher assignment

---
+++ publisher_node.cpp
@@ -0,0 +1,44 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class PublisherNode : public rclcpp::Node
+{
+public:
+  PublisherNode() : Node("publisher_node"), count_(0)
+  {
+    // Create publisher
+    publisher_ = this->create_publisher<std_msgs::msg::String>("/counter", 10);
+
+    // Create timer (500 ms)
+    timer_ = this->create_wall_timer(
+      500ms,
+      std::bind(&PublisherNode::timer_callback, this)
+    );
+  }
+
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Count: " + std::to_string(count_);
+
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
+    publisher_->publish(message);
+    count_++;
+  }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  int count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PublisherNode>());
+  rclcpp::shutdown();
+  return 0;
+}
