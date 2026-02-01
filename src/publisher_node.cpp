4b57b237e8cb68c86d197e91dbec0b0e0f9d947c
Vijay Kiran Reddy <vijayakkala06@gmail.com>
2026-02-01 15:19:32 +0530

    Remove merge conflict markers from publisher node

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-=======
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
-
->>>>>>> f65c14a398525fe73bf677bd3b39280b95472fec
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
-<<<<<<< HEAD
 class PublisherNode : public rclcpp::Node
 {
 public:
@@ -47,44 +38,5 @@ int main(int argc, char * argv[])
   rclcpp::spin(std::make_shared<PublisherNode>());
   rclcpp::shutdown();
   return 0;
-=======
-/*
- * TODO: Create a Class named 'PublisherNode' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "publisher_node".
- * 2. Create a publisher to topic "/counter" with message type std_msgs::msg::String.
- * 3. Create a timer that triggers every 500ms.
- * 4. The timer callback should:
- *    - Increment a counter (starting from 0)
- *    - Create a message with format "Count: X" where X is the counter
- *    - Publish the message
- *    - Log using RCLCPP_INFO: "Publishing: 'Count: X'"
- */
-
-class PublisherNode : public rclcpp::Node
-{
-public:
-    PublisherNode()
-        : Node("publisher_node"), count_(0)
-    {
-        // TODO: Create the publisher here
-
-        // TODO: Initialize the timer here
-    }
-
-private:
-    // TODO: Define the timer_callback function here
-
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
-};
-
-int main(int argc, char *argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PublisherNode>());
-    rclcpp::shutdown();
-    return 0;
->>>>>>> f65c14a398525fe73bf677bd3b39280b95472fec
 }
+
